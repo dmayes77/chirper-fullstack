9a3179ec947f7b5c529926159366a33f266ad008
David Mayes <dmayes77@gmail.com>
2018-08-13 21:04:55 -0400

    added privateroutes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -31,8 +31,15 @@ class App extends Component {
 								<Route exact path="/chirps" component={Home} />
 								<PrivateRoute exact path="/chirps/new" component={NewChirp} />
 								<Route exact path="/chirps/:id" component={ShowChirp} />
-								<Route exact path="/chirps/:id/edit" component={EditChirp} />
-								<Route path="/chirps/:id/delete" component={DeleteChirp} />
+								<PrivateRoute
+									exact
+									path="/chirps/:id/edit"
+									component={EditChirp}
+								/>
+								<PrivateRoute
+									path="/chirps/:id/delete"
+									component={DeleteChirp}
+								/>
 								<Route path="/chirps/users/:userid" component={UserChirps} />
 								<Route path="/login" component={Login} />
 								<Route path="/logout" component={Logout} />
